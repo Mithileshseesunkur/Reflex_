823ebfef68a04ca19b525c4dcc16b8a071e1b682
Mithileshseesunkur <zavallingum@gmail.com>
2024-06-10 15:06:18 +0200

    divider not appearing

--- Reflex_/Reflex_.py
+++ Reflex_/Reflex_.py
@@ -1,10 +1,14 @@
 import reflex as rx
+from .components.navbar import navbar
 
-def index():
+def main():
     '''return rx.text('This is Home Page', font_size="20px", colour="green", bg="blue",
                    as_="b", _hover={"colour":"white",
                                     "bg":"black"})'''
     return rx.container(
+        navbar(),
+        rx.divider(),
+
         
     )
 
@@ -13,7 +17,7 @@ def about():
     return rx.text("About page.........")
 
 app = rx.App()
-app.add_page(index)
+app.add_page(main)
 app.add_page(about, route="/aboutpage") #when added to address, reroute to about
 
 app._compile()
