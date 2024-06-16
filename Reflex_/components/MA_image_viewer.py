5b1b901a79dba9a892caa4b8883e94c1aef1f5cf
Mithileshseesunkur <zavallingum@gmail.com>
2024-06-16 21:48:19 +0200

    added checkboxes

--- Reflex_/components/MA_image_viewer.py
+++ Reflex_/components/MA_image_viewer.py
@@ -1,31 +1,76 @@
 import reflex as rx
 
-def image():
+class CheckboxState(rx.State):
 
-    return rx.chakra.grid(
-        rx.chakra.grid_item(
-            rx.chakra.image(src="/test_images/t1.png",width="512px",
-                        border_radius="15px"),
-            justify_self="start",
-            col_span=1,
-            row_span=1
+    car: bool = False
+    trafficLight: bool=False
 
+    def toggle_car_state(self):
+        self.car= not self.car
+    
+    def toggle_trafficLight_state(self):
+        self.trafficLight= not self.trafficLight
+    #option2: bool = False
+    #option3: bool = False
+    #option4: bool = False
+    #option5: bool = False
 
+def image() -> rx.Component:
 
-        ),
-        rx.chakra.grid_item(
-            rx.chakra.heading(
-                "What do you see?"
+    return rx.chakra.box( #main box
+        
+        rx.hstack( #arrange items horizontally inside main box
+            
+            rx.chakra.box( #box for image inside main box
+                
+                rx.chakra.image(src="/test_images/t1.png",
+                            width="640px",
+                            border_radius="15px",
+                            ),
+                height="100%",
+
+                width="100%"
             ),
-            col_span=1,
-            row_span=1
+            rx.chakra.box(  #box for heading and classes checkboxes
+                
+                rx.chakra.heading("What do you see?"),
+            
+                rx.divider(margin_top="10px",
+                        margin_bottom="10px",
+                ),
+
+                rx.hstack( #arrange class checkbox horizontally 
+
+                    rx.checkbox(
+                        "Car",
+                        
+                        on_change=CheckboxState.toggle_car_state()
+                        #to do something about the state of the class
+                    ),
+
+                    rx.checkbox(
+                        "Traffic light",
+                
+                        on_change=CheckboxState.toggle_trafficLight_state()
+                        #to do something about the state of the class
+                    ),
+                        align="start",
+                        spacing="4",  # Add spacing between checkboxes
+                ),
+
+            width="100%",
+
+            spacing="4",  # Add spacing between the boxes
+
+            align_items="start",  # Align items to the top
+            
+            ),
         ),
-        template_columns="repeat(2, 1fr)",
-        width="100%",
-        alignItems="center",
+        rx.divider(margin_top="20px", 
+                margin_bottom="20px", 
+                color_scheme="blue"),
 
-
-
+        width="100%"  # Ensure the outer box takes the full width
     )
     
     
